ddf5584cfad4e4630f989991d46a42b59fed308a
Sheyla-sar <sarmientosheyla.a@gmail.com>
2025-11-01 14:21:36 -0600

    Guia 5
    Ejercicios y ejemplos de la guía 5

---
+++ Guia5_20245414/js/numeroAleatorio.js
@@ -0,0 +1,34 @@
+const numeroAleatorio = Math.floor(Math.random() * 25) + 1;
+const numeroIntentos = 3;
+let intentos = 1;
+function generarNumeroAleatorio(){
+    let mensaje;
+    const parrafo = document.querySelector("#idParrafo");
+    if (intentos <= numeroIntentos){
+        let numero = prompt(
+            "¿Qué número se ha generado (Intento " + intentos + ")?"
+        );
+
+        if (numero == numeroAleatorio) {
+            mensaje = `¡Es sorprendente, pudiste adivinar el numero oculto (${numeroAleatorio}).
+            Refresque la pagina para volver a jugar.`
+        } else if (intentos == numeroIntentos){
+            mensaje = `Su numero de intentos ha termiinado.
+            El numero oculto era: ${numeroAleatorio}. Refresque la pagina para volver a jugar.`;
+        } else {
+            if (numero < numeroAleatorio) {
+                mensaje = `El número a adivinar es más alto. Quedan ${numeroIntentos - intentos} intentos.`;
+            } else {
+                mensaje = `El número a adivinar es más bajo. Quedan ${numeroIntentos - intentos} intentos.`;
+            }
+        }
+
+        intentos ++;
+
+    } else {
+        mensaje = `Su numero de intentos ha terminado.
+        El numero oculto era: ${numeroAleatorio}. Refresque la pagina para volver a jugar.`
+    }
+
+    parrafo.innerHTML = mensaje;
+}
--- Guia5_20245414/js/tablas.js
+++ Guia5_20245414/js/tablas.js
@@ -1,2 +1,33 @@
 let table = "<table>";
 table += "<thead>";
+table += "<tr>";
+table += "<th scope='col'>#</th>";
+table += "<th scope='col'>Nombre</th>";
+table += "<th scope='col'>Apellido</th>";
+table += "<th scope='col'>Correo electrónico</th>";
+table += "</tr>";
+table += "</thead>";
+table += "<tbody>";
+
+const alumnos = [
+    { id: 1, nombre: "Marcos Antonio", apellido: "Alas", correo:"marcos.alas@estudiante.org.sv"},
+    { id: 2, nombre: "Ana Paola", apellido: "Rivas Polanco", correo:"paola.rivas@estudiante.org.sv"},
+    { id: 3, nombre: "Alexis Armando", apellido: "Quintanilla Peña", correo:"alexis.quintanilla@estudiante.org.sv"},
+    { id: 4, nombre: "Vanessa Alejandra", apellido: "Bermudez Urquilla", correo:"vanessa.bermudez@estudiante.org.sv"},
+    { id: 5, nombre: "Oscar Armando", apellido: "Lopez rodriguez", correo:"oscar.lopez@estudiante.org.sv"}
+];
+
+alumnos.forEach(alumno => {
+    table += "<tr>";
+    table += `<td scope='row'>${alumno.id}</td>`;
+    table += `<td>${alumno.nombre}</td>`;
+    table += `<td>${alumno.apellido}</td>`;
+    table += `<td>${alumno.correo}</td>`;
+    table += "</tr>";
+});
+
+table += "</tbody>";
+table += "</table>";
+
+const contenedor = document.querySelector("#idContenedor");
+contenedor.innerHTML = table;
